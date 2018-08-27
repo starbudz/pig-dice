ebebf19cee9885686ed172a8aeb74c795fcbb1a2
starbudz <victorngigi@gmail.com>
2018-08-27 08:32:40 +0300

    fixed typo in html and js

--- js/scripts.js
+++ js/scripts.js
@@ -1,66 +1,70 @@
-function player(playerName, score, turnScore, turn) {
+function Player(playerName, score, turnScore, turn) {
   this.playerName = playerName;
-  this.score = score;
-  this.turnScore = turnScore;
+  this.score = 0;
+  this.turnScore = 0;
   this.turn = turn;
 }
-var dice = {
-  sides: 6,
-  roller: function() {
-    var randomNumber = Math.floor(Math.random() * this.sides) + 1;
-    return randomNumber;
-  }
-}
+var dice = roller();
+
+function roller() {
+    dice = Math.floor(6*Math.random())+1;
+
+    return dice;
+};
 var startGame = function () {
   player1 = new Player();
   player2 = new Player();
   player1.turn = true;
 }
-var activePlayer = function() {
-  if (player1.isActive === true) {
-    $("#player2panel").removeClass("activeUser");
-    $("#player1panel").addClass("activeUser");
-  } else {
-    $("#player1panel").removeClass("activeUser");
-    $("#player2panel").addClass("activeUser");
-  }
+var nextTurn = function () {
+if (player1.turn === true){
+  player1.turn = false;
+  player2.turn = true;
+  turnReset();
+} else {
+  player1.turn = true;
+  player2.turn = false;
+  turnReset();
 }
-var whichPlayerHold = function() {
-  if (player1.isActive === true) {
-    playerHold(player1);
-  } else {
-    playerHold(player2);
-  }
 }
-
-var playerHold = function(player) {
-  player.gameScore += player.turnScore;
-  switchPlayer();
-}
-player.prototype.nextTurn = function () {
-  if (player1.turn === true) {
-    player2.turn = true;
-    player1.turn = false;
-    resetTurn();
-  }
-  else {
-    player1.turn = true;
-    player2.turn = false;
-    resetTurn();
-  }
-}
-player.prototype.resetTurn = function () {
+var turnReset = function () {
   player1.turnScore = 0;
   player2.turnScore = 0;
 }
-player.prototype.winnerCheck = function () {
-  if (this.score >= 100) {
-    alert(this.player+ " You are the winner!");
+var playerRoll = function() {
+  if (player1.turn === true) {
+    rolledOne(player1);
+  } else {
+    rolledOne(player2);
+  }
+  return roll;
+}
+var playerHold = function() {
+  if (player1.turn === true) {
+    hold(player1);
+  } else {
+    hold(player2);
   }
 }
-player.prototype.hold = function() {
-  player.gameScore += player.turnScore;
-  switchPlayer();
+var rolledOne = function () {
+  var roll = dice;
+  if (roll === 1){
+    $("h3.result").text("You rolled a one. Score for this turn is zero.");
+    nextTurn();
+  } else {
+    Player.turnScore += Player.roll;
+  }
+  gameOver();
+}
+var hold = function () {
+  Player.score += Player.turnScore;
+  $("h3.result").text("Turn over. Next player ready.");
+  nextTurn();
+}
+var gameOver = function () {
+  if (Player.score >= 100){
+    $("h3.result").text("Game Over! "+ Player.playerName +"is the winner" );
+  }
 }
 
 $(document).ready(function() {
@@ -68,13 +72,10 @@ $(document).ready(function() {
         $("#intro").hide();
         $("#game").show();
         startGame();
-        activePlayer();
       });
-      $("button#roll").click(function(event) {
-        $(".dice").html('<img src=img/dice-'+ randomDie +'.png>');
-        whichPlayerRoll();
-        activePlayer();
-        var roundScore = roundScore + randomNumber();
+      $("#roll").click(function(event) {
+        $(".result").html('<img src=img/dice' + dice + '.png>');
+        playerRoll();
         $("#player1TurnScore").text(player1.turnScore);
         $("#player1score").text(player1.score);
         $("#player2TurnScore").text(player2.turnScore);
@@ -82,9 +83,8 @@ $(document).ready(function() {
         event.preventDefault();
       })
 
-      $("button#hold").click(function(event) {
+      $("#hold").click(function(event) {
         event.preventDefault();
-        whichPlayerHold();
-        activePlayer();
+        playerHold();
       })
       });
